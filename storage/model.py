7f5bbb54337eed1b861b497facfc8dbea2f1fbac
David Sanchez <e.david.sanchez.b@gmail.com>
2019-05-06 22:02:49 -0500

    Creating the storage model

---
+++ storage/model.py
@@ -0,0 +1,22 @@
+import uuid
+
+
+class Page:
+    
+    def __init__(self, name_page, address, user, password, created_at, updated_at, deleted_at, uid = None):
+        self.name_page = name_page
+        self.address = address
+        self.user = user
+        self.password = password
+        self.created_at = created_at
+        self.updated_at = updated_at
+        self.deleted_at = deleted_at
+        self.uid = uid or uuid.uuid4()
+
+
+    def to_dict(self):
+        return vars(self)
+
+    @staticmethod
+    def schema():
+        return ['uid', 'name_page', 'address', 'user', 'password', 'created_at', 'updated_at', 'deleted_at']
